8c5a58e3b527dda2d9f56541b4467d87e8cecf27
Johnny Hill <jvhill728@gmail.com>
2022-03-12 11:29:05 -0600

    postList changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import React from "react";
 import { hot } from 'react-hot-loader/root';
 import PostList from "./PostList";
 
-const App = (props) => {
-  const { name } = props;
+const App = () => {
   return (
     <>
-      <h1>Welcome, {name}</h1>
+      <h1>Strangers Things</h1>
       <PostList />
     </>
   );
