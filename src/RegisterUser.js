d48a9805a82cf24759b3c58e7018e614a1afa732
Johnny Hill <jvhill728@gmail.com>
2022-03-14 19:46:58 -0500

    changes to registerUser

--- src/RegisterUser.js
+++ src/RegisterUser.js
@@ -1,14 +1,43 @@
 import { React, useState} from 'react';
+import { registerNewUser } from './api';
 
 const RegisterUser = () => {
     const [user, setUser] = useState("");
     const [password, setPassword]= useState("");
 
+    const handleRegisterClick = () => {
+        const registerInfo = {
+            user: user,
+            password: password
+        };
+
+        registerNewUser(registerInfo);
+
+        setUser("");
+        setPassword("");
+    };
+
+    const handleUserChange = (event) => {
+        setUser(event.target.value);
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+    
     return (
       <>
         <div>
-            
+            <h1>Register New User</h1>
         </div>
+
+        <form>
+            <input type="text" value={user} onChange={handleUserChange} />
+            <input type="password" id="password" value={password}
+             onChange={handlePasswordChange} />
+            <input type="password" id="confirm-password" value={password}
+             onChange={handlePasswordChange} />
+            <button onClick={handleRegisterClick}>Register!</button>
+        </form>
       </>    
     );
 };
