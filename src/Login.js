417ee828fbc9c66a724bfed6011321b163b44ed6
Johnny Hill <jvhill728@gmail.com>
2022-03-16 20:31:41 -0500

    changes

--- src/Login.js
+++ src/Login.js
@@ -1,66 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { loginUser } from "./api"
-import PostList from "./PostList"
+import React, { useState, useEffect } from "react";
+import { loginUser } from "./api";
+import PostList from "./PostList";
+
+
 const Login = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const [user, setUser] = useState("");
-    const [password, setPassword]= useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    event.preventDefault();
 
-    const handleLogin = () => {
-        event.preventDefault();
-
-        console.log("Logging in...");
-        const registerInfo = {
-            user: user,
-            password: password
-        };
-
-        loginUser(registerInfo);
-
-        setUser("");
-        setPassword("");
+    console.log("Logging in...");
+    const registerInfo = {
+      user: user,
+      password: password,
     };
 
-    const handleUserChange = (event) => {
-        setUser(event.target.value);
-    }
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    }
+    loginUser(registerInfo);
 
-    const handleLogOut = () => {
-        localStorage.removeItem("stranger_things_JWT");
-        setIsLoggedIn(false);
-    }
-    
+    setUser("");
+    setPassword("");
+  };
 
-    useEffect(() => {
-        setIsLoggedIn(!!localStorage.getItem("stranger_things_JWT"))
-    }, []);
+  const handleUserChange = (event) => {
+    setUser(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    console.log("Is Logged In: ", isLoggedIn);
+  const handleLogOut = () => {
+    localStorage.removeItem("stranger_things_JWT");
+    setIsLoggedIn(false);
+  };
 
-    return (
-      <>
-        <div id="login">
-            <form>
-                <label>Username</label>
-                <input type="text" value={user} placeholder="Enter Username"
-                onChange={handleUserChange}
-                ></input>
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem("stranger_things_JWT"));
+  }, []);
 
-                <label>Password</label>
-                <input type ="password" value={password} placeholder="Enter Password"
-                onChange={handlePasswordChange} 
-                ></input> 
-                <button type="submit" onClick={handleLogin}>Login</button>
-                <button type="submit" onClick={handleLogOut}>Log Out</button>     
-            </form>
-        </div>
-        {isLoggedIn && <PostList />}
-      </>  
-    );
+  console.log("Is Logged In: ", isLoggedIn);
+
+  return (
+    <>
+      <div id="login">
+        <form>
+          <label>Username</label>
+          <input
+            type="text"
+            value={user}
+            placeholder="Enter Username"
+            onChange={handleUserChange}
+          ></input>
+
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter Password"
+            onChange={handlePasswordChange}
+          ></input>
+          <button type="submit" onClick={handleLogin}>
+            Login
+          </button>
+          <button type="submit" onClick={handleLogOut}>
+            Log Out
+          </button>
+        </form>
+      </div>
+      {isLoggedIn && <PostList />}
+    </>
+  );
 };
 
 export default Login;
